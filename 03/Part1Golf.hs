2e26c1f78bc5083a67d0bf175a80c795551bfca3
someguy0555 <gjonas2005@gmail.com>
2025-12-03 11:48:34 +0200

    Improved the golf only a bit.

--- 03/Part1Golf.hs
+++ 03/Part1Golf.hs
@@ -1,11 +1,13 @@
-main=getContents>>=print.sum.map((\l->x (init l)*10+x ((\a->drop 1.dropWhile(x a/=)$a) (init l)++[last l])).map g).lines
-x=maximum
-g c=read [c]
--- main=getContents>>=print.sum.map(v 0 12.map g).lines
--- u l=(x l,).(drop 1.dropWhile(x l/=))$l
--- v a n l=if n==1then s else v s(n-1)(r++(e.take(n-1).e$l))where
---  (f,r)=u$take(length l-n+1)l
---  s=a*10+f
--- e=reverse
+-- main=getContents>>=print.sum.map((\l->maximum(init l)*10+maximum((\a->drop 1.dropWhile(maximum a/=)$a)(init l)++[last l])).map(\c->read[c])).lines
+
+-- main=getContents>>=print.sum.map((\l->x(init l)*10+x((\a->drop 1.dropWhile(x a/=)$a)(init l)++[last l])).map g).lines
 -- x=maximum
 -- g c=read[c]
+
+main=getContents>>=print.sum.map(v 0 12.map g).lines
+v a n l=if n==1then s else v s(n-1)(r++(e.take(n-1).e$l))where
+ (f,r)=(\a->(x a,).(drop 1.dropWhile(x a/=))$a)$take(length l-n+1)l
+ s=a*10+f
+e=reverse
+x=maximum
+g c=read[c]

e2183a28fae83ed938bcb49487ed63d30203fa01
someguy0555 <gjonas2005@gmail.com>
2025-12-03 11:19:45 +0200

    Insane part1 golf.

--- 03/Part1Golf.hs
+++ 03/Part1Golf.hs
@@ -1,13 +1,11 @@
--- main=getContents>>=print.sum.map(v.map g).lines
+main=getContents>>=print.sum.map((\l->x (init l)*10+x ((\a->drop 1.dropWhile(x a/=)$a) (init l)++[last l])).map g).lines
+x=maximum
+g c=read [c]
+-- main=getContents>>=print.sum.map(v 0 12.map g).lines
 -- u l=(x l,).(drop 1.dropWhile(x l/=))$l
--- v l=fst(u$init l)*10+fst(u$(snd.u$init l)++[last l])
+-- v a n l=if n==1then s else v s(n-1)(r++(e.take(n-1).e$l))where
+--  (f,r)=u$take(length l-n+1)l
+--  s=a*10+f
+-- e=reverse
 -- x=maximum
 -- g c=read[c]
-main=getContents>>=print.sum.map(v 0 12.map g).lines
-u l=(x l,).(drop 1.dropWhile(x l/=))$l
-v a n l=if n==1then s else v s(n-1)(r++(e.take(n-1).e$l))where
- (f,r)=u$take(length l-n+1)l
- s=a*10+f
-e=reverse
-x=maximum
-g c=read[c]

6eccaf481f030d4b47048b62407f7f52a920a778
someguy0555 <gjonas2005@gmail.com>
2025-12-07 18:43:18 +0200

    Solved task 07
    Solved both part of the task.

---
+++ 07/Part1.lua
@@ -0,0 +1,62 @@
+
+function main()
+   local lines
+   local splits = 0
+
+   lines = get_lines()
+   local ret = get_lines_and_splits(lines)
+   lines = ret.lines
+   splits = ret.splits
+
+   -- for i, curr_line in ipairs(lines) do
+   --     print(curr_line)
+   -- end
+   print(splits)
+end
+
+function get_lines()
+    local lines = {}
+    for line in io.lines() do
+        table.insert(lines, line)
+    end
+    return lines
+end
+
+-- Source - https://stackoverflow.com/a
+-- Posted by RBerteig, modified by community. See post 'Timeline' for change history
+-- Retrieved 2025-12-07, License - CC BY-SA 3.0
+function replace_char(pos, str, r)
+    return str:sub(1, pos-1) .. r .. str:sub(pos+1)
+end
+
+function get_lines_and_splits(lines)
+   local splits = 0
+   for i, curr_line in ipairs(lines) do
+       if i > 1 then
+           local prev_line = lines[i-1]
+           for j = 1, #curr_line do
+               local char = curr_line:sub(j,j)
+               local char_above = prev_line:sub(j,j)
+
+               if char_above == '|' or char_above == 'S' then
+                   if char == '.' then
+                       curr_line = replace_char(j,curr_line,'|')
+                   elseif char == '^' then
+                       splits = splits + 1
+                       if 1 < j then
+                           curr_line = replace_char(j - 1,curr_line,'|')
+                       end
+                       if j < #curr_line then
+                           curr_line = replace_char(j + 1,curr_line,'|')
+                       end
+                   end
+               end
+           end
+           lines[i] = curr_line
+       end
+   end
+
+   return { splits = splits, lines = lines}
+end
+
+main()
---
+++ 07/Part2.lua
@@ -0,0 +1,101 @@
+
+function main()
+    local lines
+    local splits = 0
+ 
+    lines = get_lines()
+ 
+    local ret = get_lines_and_splits(lines)
+    lines = ret.lines
+    splits = ret.splits
+ 
+    -- for i, curr_line in ipairs(lines) do
+    --     for j, char in ipairs(curr_line) do
+    --         io.write(char)
+    --     end
+    --     print("")
+    -- end
+
+    local line = lines[#lines]
+    local timelines = 0
+    for i, c in ipairs(line) do
+        if type(c) == "number" then
+            -- print(c)
+            timelines = timelines + c
+        end
+    end
+    print(splits)
+    print(timelines)
+end
+
+function get_lines()
+    local lines = {}
+    for line in io.lines() do
+        if type(line) == "string" then
+            table.insert(lines, str_to_table(line))
+        end
+    end
+    return lines
+end
+
+function str_to_table(str)
+    assert(type(str) == "string")
+    t = {}
+    for i = 1, #str do
+        t[i] = str:sub(i, i)
+    end
+    return t
+end
+
+-- Source - https://stackoverflow.com/a
+-- Posted by RBerteig, modified by community. See post 'Timeline' for change history
+-- Retrieved 2025-12-07, License - CC BY-SA 3.0
+function replace_char(pos, str, r)
+    return str:sub(1, pos-1) .. r .. str:sub(pos+1)
+end
+
+function get_lines_and_splits(lines)
+    local splits = 0
+    for i, curr_line in ipairs(lines) do
+        if i > 1 then
+            local prev_line = lines[i-1]
+
+            for j, char in ipairs(curr_line) do
+                local char_above = prev_line[j]
+ 
+                if type(char_above) == "string" and char_above == 'S' then
+                    if char == '.' then
+                        curr_line[j] = 1
+                    end
+                elseif type(char_above) == "number" then
+                    if char == '.' then
+                        curr_line[j] = char_above
+                    elseif type(char) == "number" then
+                        curr_line[j] = curr_line[j] + char_above
+                    elseif char == '^' then
+                        splits = splits + 1
+                        if 1 < j then
+                            if type(curr_line[j - 1]) == "number" then
+                                curr_line[j - 1] = curr_line[j - 1] + char_above
+                            else
+                                curr_line[j - 1] = char_above
+                            end
+                        end
+                        if j < #curr_line then
+                            if type(curr_line[j + 1]) == "number" then
+                                curr_line[j + 1] = curr_line[j + 1] + char_above
+                            else
+                                curr_line[j + 1] = char_above
+                            end
+                        end
+                    end
+                end
+            end
+            lines[i] = curr_line
+        end
+    end
+ 
+    return { splits = splits, lines = lines}
+end
+
+main()
